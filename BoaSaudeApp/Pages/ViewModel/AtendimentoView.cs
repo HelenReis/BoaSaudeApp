bbfafbd7a6f838532489a9899cd46166f9cdaca0
Helen Alves dos Reis <helen.reis@mrv.com.br>
2022-01-29 15:03:48 -0300

    Melhorias

---
+++ BoaSaudeApp/Pages/ViewModel/AtendimentoView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoaSaudeApp.Pages.ViewModel
+{
+    public class AtendimentoView
+    {
+        [Required(ErrorMessage = "Campo associado é obrigatório")]
+        public string AssociadoIdSelecionado { get; set; }
+
+        [Required(ErrorMessage = "Campo procedimento é obrigatório!")]
+        public string ProcedimentoId { get; set; }
+
+        [Required(ErrorMessage = "Campo convênio é obrigatório")]
+        public string ConveniadoIdSelecionado { get; set; }
+
+        [Required(ErrorMessage = "Campo convênio é obrigatório")]
+        public DateTime Data { get; set; }
+
+        [Required(ErrorMessage = "Campo convênio é obrigatório")]
+        public DateTime Horario { get; set; }
+
+        [Required(ErrorMessage = "Campo convênio é obrigatório")]
+        public decimal Valor { get; set; }
+    }
+}
--- Dominio/AtendimentoModel/Atendimento.cs
+++ Dominio/AtendimentoModel/Atendimento.cs
@@ -11,9 +11,9 @@ namespace Dominio.AtendimentoModel
         public Atendimento(
             decimal valor,
             StatusProcessoEnum statusProcesso,
+            DateTime dataHorarioAgendamento,
             int prestadorId,
             int conveniadoId,
-            DateTime dataHorarioAgendamento,
             int associadoId,
             int cidadeId)
         {
@@ -28,14 +28,14 @@ namespace Dominio.AtendimentoModel
 
         public decimal Valor { get; private set; }
         public DateTime DataHorarioAgendamento { get; set; }
-        public StatusProcessoEnum StatusProcesso { get; private set; }
+        public StatusProcessoEnum StatusProcesso { get; set; }
         public int CidadeId { get; set; }
         public Cidade Cidade { get; set; }
-        public int AssociadoId { get; private set; }
+        public int AssociadoId { get; set; }
         public virtual Associado Associado { get; }
         public int ConveniadoId { get; private set; }
         public virtual Conveniado Conveniado { get; }
-        public int PrestadorId { get; private set; }
+        public int PrestadorId { get; set; }
         public virtual Prestador Prestador { get; }
         public int Id { get; set; }
     }
