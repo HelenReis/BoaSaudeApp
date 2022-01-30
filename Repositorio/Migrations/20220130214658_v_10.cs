1b2500852328b96d8890f11c012575868baf4cf8
Helen Alves dos Reis <helen.reis@mrv.com.br>
2022-01-30 18:55:16 -0300

    Melhorias form

---
+++ Repositorio/Migrations/20220130214658_v_10.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repositorio.Migrations
+{
+    public partial class v_10 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO ConveniadoPrestador (conveniadoId, prestadorId) VALUES " +
+                "(1, 1)");
+
+            migrationBuilder.Sql("INSERT INTO ConveniadoPrestador (conveniadoId, prestadorId) VALUES " +
+                "(1, 2)");
+
+            migrationBuilder.Sql("INSERT INTO ConveniadoPrestador (conveniadoId, prestadorId) VALUES " +
+                "(1, 3)");
+
+            migrationBuilder.Sql("INSERT INTO ConveniadoPrestador (conveniadoId, prestadorId) VALUES " +
+                "(2, 1)");
+
+            migrationBuilder.Sql("INSERT INTO ConveniadoPrestador (conveniadoId, prestadorId) VALUES " +
+                "(2, 2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
