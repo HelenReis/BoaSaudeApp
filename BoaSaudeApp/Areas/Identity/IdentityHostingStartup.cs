08cbb0b30a3ec3862076eee65e281a805ebc8818
Helen Alves dos Reis <helen.reis@mrv.com.br>
2022-02-01 18:32:36 -0300

    Incluindo páginas da UI para serem sobrescritas

---
+++ BoaSaudeApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BoaSaudeApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BoaSaudeApp.Areas.Identity.IdentityHostingStartup))]
+namespace BoaSaudeApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
