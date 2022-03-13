930fca3f38f0c17bbd391e923865c7155fa8c0f3
Helen Alves dos Reis <helen.reis@mrv.com.br>
2022-03-13 11:28:11 -0300

    Melhoria para caso de exception ao pedir localização

--- Repositorio/Migrations/20220127235855_v_8.cs
+++ Repositorio/Migrations/20220127235855_v_8.cs
@@ -6,7 +6,7 @@ namespace Repositorio.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("INSERT INTO Cidade (descricao, codigoEstado) VALUES ('Nova Serrana', 'MG')");
+            migrationBuilder.Sql("INSERT INTO Cidade (id, descricao, codigoEstado) VALUES (0, 'Não localizado', 'MG')");
             migrationBuilder.Sql("INSERT INTO Cidade (descricao, codigoEstado) VALUES ('Divinópolis', 'MG')");
             migrationBuilder.Sql("INSERT INTO Cidade (descricao, codigoEstado) VALUES ('Diamantina', 'MG')");
         }
